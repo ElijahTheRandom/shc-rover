426584bd4bd31831ded514b8985354d13c88c3f0
Areeb Mohammed <am0247@uah.edu>
2022-09-24 00:33:49 -0500

    Generated pico_relay package

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -1,34 +1,4 @@
 #include <Arduino.h>
-#include <micro_ros_platformio.h>
-
-#include <rcl/rcl.h>
-#include <rclc/rclc.h>
-#include <rclc/executor.h>
-
-#include <std_msgs/msg/int32.h>
-
-#if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
-#error This code is only meant for Arduino framework with serial transport.
-#endif
-
-rcl_publisher_t publisher;
-std_msgs__msg__Int32 msg;
-
-rclc_executor_t executor;
-rclc_support_t support;
-rcl_allocator_t allocator;
-rcl_node_t node;
-
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
-
-// Error handle loop
-void error_loop() {
-  while(1) {
-    delay(10);
-  }
-}
-
 
 #define LED_PIN 25
 
@@ -40,35 +10,13 @@ void setup() {
 
   // Configure serial transport
   Serial.begin(115200);
-  set_microros_serial_transports(Serial);
   delay(2000);
 
   // Turn LED off after serial initialization
   digitalWrite(LED_PIN, LOW);
-
-  allocator = rcl_get_default_allocator();
-
-  // Create init options
-  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-
-  // Create node
-  RCCHECK(rclc_node_init_default(&node, "pico_node", "", &support));
-
-  // Create publisher
-  RCCHECK(rclc_publisher_init_default(
-    &publisher,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "pico_count_publisher"
-  ));
-
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-
-  msg.data = 0;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(100);
-  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+  Serial.write(Serial.read());
 }
