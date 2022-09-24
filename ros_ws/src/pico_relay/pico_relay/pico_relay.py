2464f75e67e4733b1642c491696f64514369694b
Areeb Mohammed <am0247@uah.edu>
2022-09-24 00:42:15 -0500

    Created basic node and publisher outline

--- ros_ws/src/pico_relay/pico_relay/pico_relay.py
+++ ros_ws/src/pico_relay/pico_relay/pico_relay.py
@@ -1,5 +1,20 @@
-def main():
-    print('Hi from pico_relay.')
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+class SerialPublisher(Node):
+    def __init__(self):
+        super().__init__("serial_publisher")
+        self.publisher = self.create_publisher(String, '/pico/serial', 10)
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    serial_pub = SerialPublisher()
+
+    rclpy.spin(serial_pub)
 
 
 if __name__ == '__main__':
