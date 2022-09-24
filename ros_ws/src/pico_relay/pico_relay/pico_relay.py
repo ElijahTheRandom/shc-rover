72b26067aa2c95330f2a0240c65918a8aa4e136b
Areeb Mohammed <am0247@uah.edu>
2022-09-24 00:35:10 -0500

    Made pico_relay node files

---
+++ ros_ws/src/pico_relay/pico_relay/pico_relay.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from pico_relay.')
+
+
+if __name__ == '__main__':
+    main()
--- ros_ws/src/pico_relay/setup.py
+++ ros_ws/src/pico_relay/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'pico_relay = pico_relay.pico_relay:main'
         ],
     },
 )
